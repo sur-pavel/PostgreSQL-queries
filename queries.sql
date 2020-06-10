8e710673b1ae242983181ea398f37edeace73e4f
Pavel <sur-pavel@mail.ru>
2020-06-10 10:58:32 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,23 @@
+﻿SELECT group_id, COUNT(*) count FROM students
+	GROUP BY group_id
+	HAVING COUNT(*) < 10;
+
+DELETE FROM courses_students
+WHERE student_id = 
+	(SELECT student_id FROM students 
+	WHERE group_id =
+		(SELECT group_id FROM groups
+		WHERE name = 'SR-01')); 
+
+DELETE FROM students 
+WHERE group_id =
+	(SELECT group_id FROM groups 
+	WHERE name = 'SR-01'); 
+	
+SELECT co.name, st.first_name, st.last_name
+FROM courses AS co
+INNER JOIN courses_students AS cost
+	ON co.course_id = junc.course_id
+INNER JOIN students AS st
+	ON st.student_id = junc.student_id;
+	 
---
+++ tables creation.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE groups (
+	group_id SERIAL PRIMARY KEY,
+	name CHAR(20) NOT NULL
+);
+
+CREATE TABLE students (
+	student_id SERIAL PRIMARY KEY,
+	group_id int NOT NULL,
+	first_name CHAR(20) NOT NULL,
+	last_name CHAR(20) NOT NULL,
+	FOREIGN KEY (group_id) REFERENCES groups
+);
+
+CREATE TABLE courses (
+	course_id SERIAL PRIMARY KEY,
+	name CHAR(20) NOT NULL,
+	description CHAR(50)
+);
+
+CREATE TABLE courses_students (
+	course_id int NOT NULL,
+	student_id int NOT NULL,
+	PRIMARY KEY (course_id, student_id),
+	FOREIGN KEY (course_id) REFERENCES courses,
+	FOREIGN KEY (student_id) REFERENCES students
+);
+ 
